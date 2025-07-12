d7b7e620046d6b494189bf462a5a6be185980149
Jaleed khan <132694164+JaleedKhanofficial@users.noreply.github.com>
2025-07-12 15:41:06 +0500

    Add job listings and contact page with layout components
    - Updated the home route to return the 'home' view.
    - Added a contact route that returns the 'contact' view.
    - Created a jobs route that returns a list of job positions with titles and salaries.
    - Implemented a dynamic job detail route to display individual job information.
    - Added layout component for consistent page structure.
    - Created navigation link component for easier link management.
    - Developed views for home, contact, job listings, and individual job details.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,72 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+
+route::get('/contact', function(){
+    return view('contact');
+});
+
+Route::get('/jobs',function (){
+    return view('jobs',[
+        'jobs' => [
+            [
+                'id' =>1,
+                'title' => 'Director',
+                'salary'=> '$1000'   
+            ],
+            [
+                'id'=>2,
+                'title' => 'Manager',
+                'salary'=> '$800'   
+            ],
+            [
+                'id'=> 3,
+                'title' => 'Developer',
+                'salary'=> '$600'   
+            ],
+            [
+                'id'=> 4,
+                'title' => 'Designer',
+                'salary'=> '$500'   
+            ]
+        ]
+    ]);
+});
+
+
+Route::get('/jobs/{id}', function($id){
+    $jobs = [
+        [
+            'id'=> 1,
+            'title'=> 'Director',
+            'salary'=> '$1000'
+        ],
+        [
+            'id'=> 2,
+            'title'=> 'Manager',
+            'salary'=> '$800'
+        ],
+        [
+            'id'=> 3,
+            'title'=> 'Developer',
+            'salary'=> '$600'
+        ],
+        [
+            'id'=> 4,
+            'title'=> 'Designer',
+            'salary'=> '$500'
+        ]
+    ];
+    
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    // dd($job);
+    
+
+    return view('job', ['job' => $job]);
 });
